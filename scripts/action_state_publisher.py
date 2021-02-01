0765c8c2569d805a618faf8d731570e3b1b422cc
Yudha Pane <yudha.pane@kuleuven.be>
2021-02-01 19:10:31 +0100

    started working on the action publisher

--- scripts/action_state_publisher.py
+++ scripts/action_state_publisher.py
@@ -5,19 +5,46 @@ from std_msgs.msg import String
 from task_planner.msg import Action
 from task_planner.msg import Monitor
 
+pub = rospy.Publisher('action_status', action, queue_size=10)
+msg = Action()
+collision_monitor = Monitor()
+admittance_monitor = Monitor()
+last_action_left_arm = "none"
+last_action_right_arm = "none"
+
+def callback_action(data):
+    #rospy.loginfo("%s " % data.data)
+    # parse data
+    str = data.data[2:-10]
+    msg.id = str
+    collision_monitor.predicate = "collision_free"
+    collision_monitor.arguments = ["left_arm", "obstacle"]
+    admittance_monitor.predicate = "admittance_free"
+    admittance_monitor.arguments = ["left_arm", "obstacle"]
+    msg.monitors  = [collision_monitor, admittance_monitor]
+    msg.succeed   = True
+    pub.publish(msg)
+
+def callback_collision(data):
+    print("here")
+
+def callback_admittance(data):
+    print("here")
 
 def talker():
-    pub = rospy.Publisher('action', Action, queue_size=10)
-    rospy.init_node('tamp_interface', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-
-    msg = Action()
-    mon = Monitor()
     mon.predicate = "collision_free"
     mon.arguments = ["left_arm", "obstacle"]
     msg.id = "move_above_left_arm_slot11_slot21"
     msg.monitors = [mon]
     msg.succeed = False
+    
+    
+    pub = rospy.Publisher('action', Action, queue_size=10)
+    rospy.Subscriber("eventPort", String, callback_action)    
+    rospy.Subscriber("collision_status", String, callback_collision)    
+    rospy.Subscriber("callback_admittance", String, callback_admittance)    
+    rospy.init_node('action_monitor_publisher', anonymous=True)
+    rate = rospy.Rate(100) # 10hz
 
     while not rospy.is_shutdown():
         #rospy.loginfo(msg)
