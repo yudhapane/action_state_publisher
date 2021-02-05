d81ea1219e57618f3259e6cd74e409fcc5249148
Vahid Mokhtari <mokhtari.vahid@kuleuven.be>
2021-02-05 11:54:56 +0100

    upgraded task_planner_interface

--- scripts/task_planner_interface.py
+++ scripts/task_planner_interface.py
@@ -24,6 +24,15 @@ def parse():
 
     parser.add_argument('problem', type=str, help='name of a PDDL problem file', 
         choices=("prob1_1", "prob2_1", "prob3_1", "prob4_1", "prob1_0", "prob2_0", "prob3_0", "prob4_0"))
+    parser.add_argument("-a", "--agents", nargs='+', type=str, default=["left_arm", "right_arm"], 
+        help="a list of agents: e.g., -a left_arm right_arm")
+    parser.add_argument("-c", "--planners", nargs='+', type=str, default=["ff"], 
+        choices=os.listdir('planners'), metavar='PLNNER', 
+        help="a list of classical planners: ff, fd, m, prob, optic-clp, lpg-td, lpg, vhpop (e.g. -c ff fd m) (default=[optic-clp])")
+    parser.add_argument("-t", "--temporal_actions", nargs='+', type=str, default=["avoid_collision", "admittance_control"], 
+        help="a list of temporal (durative) actions: e.g., -t avoid_collision admittance_control")
+    parser.add_argument("-r", "--rank", help="to disable ranking the compiled classical planning domains \
+        by higher probabilistic outcomes (default=True)", action="store_true", default=False)
     parser.add_argument("-v", "--verbose", default=0, type=int, choices=(0, 1, 2),
         help="increase output verbosity: 0 (minimal), 1 (high-level), 2 (classical planners outputs) (default=0)", )
 
@@ -35,15 +44,15 @@ def parse():
 
 def send_json_plan(plan_json_file, actions_json_file):
 
+    # a list keeping the ids of successfully executed actions
+    global action_ids
+    action_ids = []
+
     plan_json_file = os.path.splitext(os.path.splitext(plan_json_file)[0])[0]
     pub = rospy.Publisher('plan', String, queue_size=10, latch=True)
     rospy.init_node('tamp_interface', anonymous=True)
     pub.publish(plan_json_file)
     print(color.fg_yellow('\n - path to the json files: ') + '{}'.format(plan_json_file))
-
-    # a list keeping the ids of successfully executed actions
-    global action_ids
-    action_ids = []
 
     # pub = rospy.Publisher('plan', Plan, queue_size=10)
     # rospy.init_node('tamp_interface', anonymous=True)
@@ -58,26 +67,16 @@ def send_json_plan(plan_json_file, actions_json_file):
 #############################
 #############################
 
-# def action_execution_verification(action_msg, (problem, plan, state, goals, verbose)):
 def action_execution_verification(action_msg):
 
-    global problem, plan, state, goals
-
-    # rospy.loginfo(rospy.get_caller_id() + ': %s %s %s', action_msg.id, action_msg.succeed, action_msg.monitors)
+    global domain, problem, state, plan, action_ids
 
     #############################
     ## simulate and execute the plan
     for level, step in plan.items():
 
-        # if step == 'GOAL':
-        #     # goal state is achieved
-        #     print(color.fg_voilet('@ GOAL'))
-        #     sub_proc.unregister()
-        #     rospy.signal_shutdown('finished')
-        #     return
-
         ## check if goal is achieved 
-        if state.is_true(goals):
+        if state.is_true(problem.goals) or step == 'GOAL':
             # goal state is achieved
             print(color.fg_voilet('@ GOAL'))
             sub_proc.unregister()
@@ -85,7 +84,7 @@ def action_execution_verification(action_msg):
             return
 
         else:
-            # unfold step into a tuple of actoins and outcomes
+            # unfold step into a tuple of actions and outcomes
             (actions, outcomes) = step
 
             for action in actions:
@@ -104,7 +103,6 @@ def action_execution_verification(action_msg):
                         print(color.fg_yellow(' + ') + str(action))
                         # apply action to the state and update the state
                         state = state.apply(action)
-                        return
                     else:
                         ## print out failed action
                         print(color.fg_red(' - ') + str(action))
@@ -131,43 +129,26 @@ def action_execution_verification(action_msg):
                         ## convert back to frozenset
                         state.predicates = frozenset(state_predicates)
 
-                    break
+                        #############################
+                        ## create a new pddl problem file at /tmp/safe-planner
+                        problem_pddl = pddl.pddl(problem, state=state)
 
-            # inner for-loop finished with no break
-            else:
-                continue
-            # inner for-loop was broken, then break outer for-loop too
-            break
+                        ## call planner to make an initial policy given the domain, problem and planners
+                        (policy, plan, plan_json_file, actions_json_file) = \
+                            make_plan(domain = domain, \
+                                      problem = problem_pddl, \
+                                      planners = args.planners, \
+                                      agents = args.agents, \
+                                      temporal_actions = args.temporal_actions, \
+                                      rank=False, \
+                                      verbose=args.verbose)
 
-    # ## check if goal is achieved 
-    # if state.is_true(goals):
-    #     sub_proc.unregister()
-    #     rospy.signal_shutdown('finished')
-    #     print(color.fg_yellow('@ GOAL'))
-    #     return
+                        #############################
+                        ## call the execution engine giving the initial plan
+                        # plan_json_file actions_json_file
+                        send_json_plan(plan_json_file, actions_json_file)
 
-    # for-loop was broken due to execution failure, then make a new plan and continue
-    #############################
-    ## create a new pddl problem file at /tmp/safe-planner
-    problem_pddl = pddl.pddl(problem, state=state, goal=goals)
-
-    ## set the default solenoid domain parameters
-    ## call planner to make an initial policy given the domain, problem and planners
-    (policy, plan, plan_json_file, actions_json_file) = \
-        make_plan(domain = domain, \
-                  problem = problem_pddl, \
-                  planners = planners, \
-                  agents = agents, \
-                  temporal_actions = temporal_actions, \
-                  rank=False, \
-                  verbose=verbose)
-
-    #############################
-    ## call the execution engine giving the initial plan
-    # plan_json_file actions_json_file
-    send_json_plan(plan_json_file, actions_json_file)
-
-    return
+                    return
 
 
 #############################
@@ -179,9 +160,9 @@ if __name__ == '__main__':
     # change to the task planner directory
     os.chdir(os.path.split(__file__)[0])
 
-    args = parse()
+    global args, domain, problem, state, plan, action_ids
 
-    global domain, problem, planners, agents, temporal_actions, verbose, plan, state, goals, action_ids
+    args = parse()
 
     ## parse domain and create a domain object
     problem = pddlparser.PDDLParser.parse("benchmarks/multirob/solenoid/" + args.problem + ".pddl")
@@ -190,17 +171,12 @@ if __name__ == '__main__':
     ## in any geometric limitation, the goal is updated to recover from 
     ## the failure situation (we do not plan again to the original goal)
     state = problem.initial_state
-    goals = problem.goals
 
     ## create a new pddl problem file at /tmp/safe-planner
-    problem_pddl = pddl.pddl(problem, state=state, goal=goals)
+    problem_pddl = pddl.pddl(problem, state=state)
 
-    ## set the default solenoid domain parameters
+    ## set the default solenoid domain 
     domain = "benchmarks/multirob/solenoid/domain.pddl"
-    planners = ["optic-clp"]
-    agents = ("left_arm", "right_arm")
-    temporal_actions = ("avoid_collision", "admittance_control")
-    verbose = args.verbose
 
     #############################
     ## set the default solenoid domain parameters
@@ -208,11 +184,11 @@ if __name__ == '__main__':
     (policy, plan, plan_json_file, actions_json_file) = \
         make_plan(domain = domain, \
                   problem = problem_pddl, \
-                  planners = planners, \
-                  agents = agents, \
-                  temporal_actions = temporal_actions, \
+                  planners = args.planners, \
+                  agents = args.agents, \
+                  temporal_actions = args.temporal_actions, \
                   rank=False, \
-                  verbose=verbose)
+                  verbose=args.verbose)
 
     #############################
     ## call the execution engine giving the initial plan
